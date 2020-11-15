9309c4c49304b901879261030fa846c16d640913
Duduling <duduling20@gmail.com>
2020-11-15 18:11:32 +0900

    #3.1 Apollo Setup part One

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+
 const rootNode = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   rootNode);
