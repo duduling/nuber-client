f8553ad96bc754d47a6716be7a5cd2a916ebe8e3
Duduling <duduling20@gmail.com>
2020-11-19 08:35:51 +0900

    #3.14 Route Components Review

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { graphql } from 'react-apollo'
 
-import theme from '../../style/theme'
-import { ThemeProvider } from '../../style/typed-components'
+import theme from 'style/theme'
+import { ThemeProvider } from 'style/typed-components'
 
 import AppPresenter from './AppPresenter'
 import { IS_LOGGED_IN } from './AppQueries'
 
-
 const AppContainer: React.FC<any> = ({ data }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
-        </ThemeProvider>
-    )
+	return (
+		<ThemeProvider theme={theme}>
+			<AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+		</ThemeProvider>
+	)
 }
 
 export default graphql(IS_LOGGED_IN)(AppContainer)
